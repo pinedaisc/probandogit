ad825a4f47335ce862498921e52e12845d88a666
Miguel Ernesto Pineda Esteva <mpinedae@bancoazteca.com>
2019-07-23 11:50:27 -0500

    hola mundo cruel

--- src/probandogit/com/IniciandoGit.java
+++ src/probandogit/com/IniciandoGit.java
@@ -2,6 +2,6 @@ package probandogit.com;
 
 public class IniciandoGit {
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+		System.out.println("Hola mundo cruel");
 	}
 }
