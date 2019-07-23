0dca252160d3efea616aa9dcd97e7f71f2587fde
Miguel Ernesto Pineda Esteva <mpinedae@bancoazteca.com>
2019-07-23 11:58:42 -0500

    funcion getNombre

--- src/probandogit/com/IniciandoGit.java
+++ src/probandogit/com/IniciandoGit.java
@@ -2,6 +2,10 @@ package probandogit.com;
 
 public class IniciandoGit {
 	public static void main(String[] args) {
-		System.out.println("Hola mundo cruel");
+		System.out.println("Hola"+ getNombre());
+	}
+	
+	public static String getNombre() {
+		return "Manuel";
 	}
 }
