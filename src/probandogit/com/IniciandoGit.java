953a7c0735046bb974ca9bc52e3c6fa1586d7bc0
Miguel Ernesto Pineda Esteva <mpinedae@bancoazteca.com>
2019-07-24 14:20:28 -0500

    agrego metodo dia que cae cumpleaños(ingles)

--- src/probandogit/com/IniciandoGit.java
+++ src/probandogit/com/IniciandoGit.java
@@ -5,6 +5,7 @@ import probandogit.com.util.Utils;
 public class IniciandoGit {
 	public static void main(String[] args) {
 		System.out.println("Hola"+ Utils.getNombre());
+		System.out.println("Tu cumpleaños será el día : " + Utils.getDiaCumple(2018, 9, 5));
 	}
 	
 	
--- src/probandogit/com/util/Utils.java
+++ src/probandogit/com/util/Utils.java
@@ -1,5 +1,7 @@
 package probandogit.com.util;
 
+import java.time.LocalDate;
+
 public class Utils {
 
 	public static String getFecha() {
@@ -11,5 +13,9 @@ public class Utils {
 	
 	public static String getApellido() {
 		return "Pineda";
+	}
+	
+	public static String getDiaCumple(int año, int mes, int dia) {
+		return LocalDate.of(año, mes, dia).getDayOfWeek().toString();
 	}
 }
